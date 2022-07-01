05fa2412fd36283f02334a646e87fcdc72adf0c8
Josh <jlee12297@gmail.com>
2022-06-30 21:30:37 -0700

    project is basically completely functional. finally tweaking and making the page prettier needed

--- script.js
+++ script.js
@@ -5,9 +5,11 @@ var firstChoice = document.querySelector('#choice1');
 var secondChoice = document.querySelector('#choice2');
 var thirdChoice = document.querySelector('#choice3');
 var fourthChoice = document.querySelector('#choice4');
-var scoreSpan = document.querySelector('#scoreSpan');
+var scoreList = document.querySelector('#scoreList');
 var timeLeftSpan = document.querySelector('#timeLeftSpan');
 var timeLeft
+//array to store past scores so we can render them onto the page upon reload
+var storedScores = [];
 
 // questions array
 
@@ -19,10 +21,43 @@ question5 = {question:"Which event occurs when the user clicks on an HTML elemen
 allquestions = [question1, question2, question3, question4, question5]
 answersheet = [3,3,3,2,1]
 questionIndex = 0
-// currentQuestion = 0
-// timeLeft
-// score
-// highscores
+
+//function to render the stored scores onto the page as <li> elements in the designated score box
+    function renderStoredScores() {
+        scoreList.innerHTML= "";
+
+          // Render a new li for each stored score
+  for (var i = 0; i < storedScores.length; i++) {
+    var savedScore = storedScores[i];
+
+    var li = document.createElement("li");
+    li.textContent = savedScore;
+    li.setAttribute("data-index", i);
+
+    scoreList.appendChild(li);
+  }
+
+ }
+
+// This function is being called below and will run when the page loads.
+    function init() {
+    // Get storedScores from localStorage
+     var getStoredScores = JSON.parse(localStorage.getItem("storedScores"));
+  
+    // If scores were retrieved from localStorage, update the storedScores array to it
+     if (getStoredScores !== null) {
+      storedScores = getStoredScores;
+    }
+  
+    // This is a helper function that will render storedScores to the DOM
+     renderStoredScores();
+  }
+  
+     function saveScore() {
+    // Stringify and set key in localStorage to storedScores array
+      localStorage.setItem("storedScores", JSON.stringify(storedScores));
+  }
+  
 
 // startGame
     // triggered by event listener on start game button
@@ -35,7 +70,7 @@ questionIndex = 0
         timeLeftSpan.textContent=timeLeft
         if(timeLeft === 0){
             // if time runs out, lose
-            console.log("GAME OVER")
+            timeLeftSpan.textContent="GAME OVER"
             clearInterval(timer);
         }
     },1000)
@@ -45,40 +80,68 @@ questionIndex = 0
     // loadQuestion()
     function loadQuestion(qIndex){
         question.textContent = allquestions[qIndex].question;
-        firstChoice.textContent = allquestions[qIndex].ans[0] //allquestions[qIndex].ans1;
-        secondChoice.textContent = allquestions[qIndex].ans[1] //allquestions[qIndex].ans2;
-        thirdChoice.textContent = allquestions[qIndex].ans[2] //allquestions[qIndex].ans3;
-        fourthChoice.textContent = allquestions[qIndex].ans[3] //allquestions[qIndex].ans4;
+        firstChoice.textContent = allquestions[qIndex].ans[0] 
+        secondChoice.textContent = allquestions[qIndex].ans[1]
+        thirdChoice.textContent = allquestions[qIndex].ans[2]
+        fourthChoice.textContent = allquestions[qIndex].ans[3]
 
     }
-    /*
-     * Function: Compares the correct answer with the user selected answer
-     * Param 1 qIndex: Correct Answer
-     * PAram 2 userChoice: Choice of user 
-     * returns: true or false 
-    */
+
     function checkAnswer(qIndex,userChoice){
-        //answer = allquestions[qIndex].[answersheet[qIndex]]
         answer = allquestions[qIndex].ans[answersheet[qIndex]];
         console.log(answer)
-        // if statement compare userChoice and answer
-        // 
+        if (answer === userChoice){
+            console.log("CORRECT");
+            questionIndex++
+        } else {
+            console.log("INCORRECT");
+            questionIndex++;
+            timeLeft -= 10;
+        }
+        if (questionIndex < 5){
+        loadQuestion(questionIndex);
+        } else {
+            setScoreBoard();
+            clearInterval(timer);
+            timeLeftSpan.textContent = "Congrats, you answered all questions! Check your score.";
+            document.getElementById("timeBox").hidden = true;
+    }}
+
+
+    function setScoreBoard(){
+        //take remaining timeLeft as the score, make a popup window to take the players initials, then put it into a scoreboard, where it is locally stored
+       
+        var score = timeLeft;
+        listItem = document.createElement('li');
+        
+        initials = prompt("Please type your gamertag so we can save your score!")
+        listItem.textContent = initials + "  got " + score +  " points!"
+        var scoreText = listItem.textContent
+        storedScores.push(scoreText);
+        
+        saveScore();
+        renderStoredScores();
     }
 
     firstChoice.addEventListener("click", function() {
         // retrieve firstChoice text in a variable
-        checkAnswer(questionIndex, "");
+        userChoice = firstChoice.textContent;
+        checkAnswer(questionIndex, userChoice);
     })
 
     secondChoice.addEventListener("click", function() {
-
+        userChoice = secondChoice.textContent;
+        checkAnswer(questionIndex, userChoice);
     })
 
     thirdChoice.addEventListener("click", function() {
-
+        userChoice = thirdChoice.textContent;
+        checkAnswer(questionIndex, userChoice);
     })
 
     fourthChoice.addEventListener("click", function() {
-
+        userChoice = fourthChoice.textContent;
+        checkAnswer(questionIndex, userChoice);
     })
-   
+
+   init()
